ecc1e1860275a5c817f872095ddd26c47d035d8a
2ㅣ2ㅣ <106798875+cseeeeee@users.noreply.github.com>
2024-05-09 14:35:22 +0900

    chore: trigger github-action

--- src/main/java/com/example/demo1/Demo1Application.java
+++ src/main/java/com/example/demo1/Demo1Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo1Application {
 
 //	그냥 커밋해보기
+//	ㅎㅎ
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
 	}
